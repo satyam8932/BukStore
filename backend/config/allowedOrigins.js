a555d71f6b13c79f88d5424ba057ffcf05dab656
Satyam Singh <satyamsingh5august@gmail.com>
2024-04-12 13:06:36 +0530

    api config

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://bukstore.vercel.app/"
+    "https://bukstore.vercel.app/",
+    "https://bukstore.vercel.app",
 ]
 
 export default allowedOrigins;
